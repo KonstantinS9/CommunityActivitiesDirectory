d7f4b7db65c84061f87adca90a0302afee451c1e
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-21 21:27:53 +0200

    Added folder "Common" with class "EntityValidation" for my entites validation.

---
+++ CommunityActivitiesDirectory/Common/EntityValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace CommunityActivitiesDirectory.Common
+{
+    public static class EntityValidation
+    {
+        /* Activity */
+        public const int ActivityTitleMaxLength = 100;
+        public const int ActivityTitleMinLength = 3;
+        public const int ActivityDescriptionMaxLength = 1000;
+        public const int ActivityDescriptionMinLength = 1;
+        public const int ActivityLocationMaxLength = 200;
+        public const int ActivityLocationMinLength = 2;
+        public const int ActivityOrganizerMaxLength = 200;
+        public const int ActivityOrganizerMinLength = 2;
+    }
+}
