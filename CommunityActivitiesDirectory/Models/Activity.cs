dfea47a7e2b0e2b610c531e29126bcfdeb4c8706
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-21 21:33:36 +0200

    Used data annotations for my "Activity" entity

--- CommunityActivitiesDirectory/Models/Activity.cs
+++ CommunityActivitiesDirectory/Models/Activity.cs
@@ -1,15 +1,34 @@
-﻿namespace CommunityActivitiesDirectory.Models
+﻿using System.ComponentModel.DataAnnotations;
+using static CommunityActivitiesDirectory.Common.EntityValidation;
+namespace CommunityActivitiesDirectory.Models
 {
     public class Activity
     {
+        [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Location { get; set; }
+
+        [Required]
+        [MaxLength(ActivityTitleMaxLength)]
+        [MinLength(ActivityTitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ActivityDescriptionMaxLength)]
+        [MinLength(ActivityDescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ActivityLocationMaxLength)]
+        [MinLength(ActivityLocationMinLength)]
+        public string Location { get; set; } = null!;
         public DateTime Date { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
-        public string Organizer { get; set; }
+
+        [Required]
+        [MaxLength(ActivityOrganizerMaxLength)]
+        [MinLength(ActivityOrganizerMinLength)]
+        public string Organizer { get; set; } = null!;
         public bool IsPublic { get; set; }
         public int MaxParticipants { get; set; }
     }
