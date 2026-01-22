901277f9d9487b2cd38c54a3d2c0dbc40342d712
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-22 18:59:45 +0200

    Added my tables in the dbContext

--- CommunityActivitiesDirectory/Data/ApplicationDbContext.cs
+++ CommunityActivitiesDirectory/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CommunityActivitiesDirectory.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CommunityActivitiesDirectory.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public virtual DbSet<Activity> Activities { get; set; } = null!;
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Venue> Venues { get; set; } = null!;
     }
 }
